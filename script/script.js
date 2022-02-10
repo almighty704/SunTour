d24a3a5438a1ff0c3c1e7c08aaedb46fbc656d7e
almighty704 <klajvertcela@hotmail.com>
2022-02-10 10:35:57 +0100

    last

---
+++ script/script.js
@@ -0,0 +1,16 @@
+
+
+const toggle = document.querySelector('.toggle');
+const addToggle = document.querySelector('.display')
+
+
+
+toggle.addEventListener('click', e => {
+  addToggle.classList.toggle('nav-toggle');
+
+  });
+
+
+
+
+  
